c6fde7daa4f532084174cfe1b7092475ad137a3e
olamilekan5162 <opeyemiolalekan481@gmail.com>
2025-06-06 23:25:49 +0100

    add my thought process

--- scripts/script.js
+++ scripts/script.js
@@ -69,28 +69,37 @@ const mainMenu = () => {
     search();
   } else if (userChoice === "2") {
     themeList();
+  } else {
+    mainMenu();
   }
 };
 
-// thought process for searching
-// 1. i prompt the user to enter the theme name of the story they are looking for
+// Thought process for searching
+// 1. I prompt the user to enter the theme name of the story they are looking for
 // 2. I loop through the story collection array and check if the array contains the theme name searched by the user
-// 3. if it is found i log all story with the theme name in a table
+// 3. if it is found I log all story with the theme in a table
+// 4. then i use an else statement to print "No matching story with the theme name" if the theme is not found but i found out it prints the else statement the amount of time it does not find the theme while looping through the array
+// 5. so i resolved to introducing a boolen isFound that gets toggle if at least a single story with the theme is found, thus i can leverage on that to prevent the loop from printing "No matching story with the theme name" even when a story is found
 
 const search = () => {
   const theme = prompt("Search for story by theme name ...");
-  let isfound = false;
+  let isFound = false;
 
   for (let i of storyCollection) {
     if (i.theme.toLocaleLowerCase() === theme.toLocaleLowerCase()) {
-      isfound = true;
+      isFound = true;
       console.table(i);
     }
   }
-  if (!isfound) {
+  if (!isFound) {
     console.error("No matching story with the theme name");
   }
 };
+
+// Thought process for implementing categorizing story by theme
+// 1. I initialize an empty object to store the theme and it count as a key and value
+// 2. I loop through the story collection array and store all theme as a key and an initial value of 1
+// 3. While looping, I check if a theme is already in the initialized object, if it already exist, i increment it value by 1 and the increament goes on as long as the loop hits a theme that is already in the intialized object again and again.
 
 const themeList = () => {
   const themeCount = {};
